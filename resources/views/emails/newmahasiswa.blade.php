af924d93ee39bc0116c9b234ab63849c0f2e7bdf
tedo <tedohac@gmail.com>
2021-07-15 16:11:43 +0000

    20210715 13

--- resources/views/emails/newmahasiswa.blade.php
+++ resources/views/emails/newmahasiswa.blade.php
@@ -2,33 +2,37 @@
 # Selamat datang di MagangHub!
 Admin kampus <b>{{ $param['univ_nama'] }}</b> telah mendaftarkan anda sebagai mahasiswa di MagangHub. Berikut identitas anda:<br />
 <table>
-    <tr>
-        <td width="10%">Kampus</td>
-        <td>: {{ $param['univ_nama'] }}</td>
-    </tr>
-    <tr>
-        <td>Program Studi</td>
-        <td>: {{ $param['prodi_nama'] }}</td>
-    </tr>
-    <tr>
-        <td>NIM</td>
-        <td>: {{ $param['request']->mahasiswa_nim }}</td>
-    </tr>
-    <tr>
-        <td>Nama Mahasiswa</td>
-        <td>: {{ $param['request']->mahasiswa_nama }}</td>
-    </tr>
+    <tbody>
+        <tr>
+            <td width="10%">Kampus</td>
+            <td>: {{ $param['univ_nama'] }}</td>
+        </tr>
+        <tr>
+            <td>Program Studi</td>
+            <td>: {{ $param['prodi_nama'] }}</td>
+        </tr>
+        <tr>
+            <td>NIM</td>
+            <td>: {{ $param['request']->mahasiswa_nim }}</td>
+        </tr>
+        <tr>
+            <td>Nama Mahasiswa</td>
+            <td>: {{ $param['request']->mahasiswa_nama }}</td>
+        </tr>
+    </tbody>
 <table><br />
 Dan berikut informasi login untuk akun anda:<br /><br />
 <table>
-    <tr>
-        <td width="50">e-mail</td>
-        <td>: {{ $param['request']->mahasiswa_user_email }}</td>
-    </tr>
-    <tr>
-        <td>Password</td>
-        <td>: {{ $param['password'] }}</td>
-    </tr>
+    <tbody>
+        <tr>
+            <td width="50">e-mail</td>
+            <td>: {{ $param['request']->mahasiswa_user_email }}</td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td>: {{ $param['password'] }}</td>
+        </tr>
+    </tbody>
 <table><br />
 Untuk mulai menggunakan akun anda, langkah selanjutnya adalah mengaktifkan akun dengan cara klik pada tombol di bawah ini. Kemudian anda dapat langsung login menggunakan informasi di atas.<br>
 
