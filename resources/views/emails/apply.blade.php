30f4f1b07b9a1f9e70351d85404fe7c425e24999
tedo <tedohac@gmail.com>
2021-07-15 15:08:00 +0000

    20210715

---
+++ resources/views/emails/apply.blade.php
@@ -0,0 +1,28 @@
+@component('mail::message')
+# Lamaran Magang Baru
+Anda mendapatkan lamaran magang baru atas lowongan:<br />
+{{ $param['lowongan']->lowongan_judul }}
+<br /><br />
+Dari Mahasiswa:
+<table>
+    <tr>
+        <td>Kampus</td>
+        <td>: {{ $param['mahasiswa']->univ_nama }}</td>
+    </tr>
+    <tr>
+        <td>Prodi</td>
+        <td>: {{ $param['mahasiswa']->prodi_nama }}</td>
+    </tr>
+    <tr>
+        <td>Mahasiswa</td>
+        <td>: {{ $param['mahasiswa']->mahasiswa_nama }}</td>
+    </tr>
+<table><br>
+
+@component('mail::button', ['url' => $param['url']])
+Lihat Detail
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
